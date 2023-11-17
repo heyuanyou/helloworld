3473a496161676b3bdcb17b9ca68c860a934fd5a
heyuanyou <harrell16805@163.com>
2023-11-17 10:35:07 +0800

    print log

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,6 +10,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
+		fmt.Println("ping...")
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
